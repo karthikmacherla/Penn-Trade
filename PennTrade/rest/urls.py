2128f901db1a5e6fda529c2632d79f834b849408
Karthik Macherla <kmacherla@datamaxx.corp>
2019-07-01 17:03:33 -0400

    updated settings to account for django_rest

--- PennTrade/rest/urls.py
+++ PennTrade/rest/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ProductList, ProductDetail
 
 urlpatterns = [
-    path('product/', ProductList.as_view, name="product_list"),
-    path('product/<int:pk>', ProductDetail.as_view, name="product_detail"),
+    path('product/', ProductList.as_view(), name="product_list"),
+    path('product/<int:pk>', ProductDetail.as_view(), name="product_detail"),
 ]
