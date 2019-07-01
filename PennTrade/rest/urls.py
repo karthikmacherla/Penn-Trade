e555e28b38d604d0bfbcaed100b7894d931c56fb
Karthik Macherla <kmacherla@datamaxx.corp>
2019-07-01 16:50:51 -0400

    added urls

--- PennTrade/rest/urls.py
+++ PennTrade/rest/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import ProductList, ProductDetail
 
 urlpatterns = [
-    
+    path('product/', ProductList.as_view, name="product_list"),
+    path('product/<int:pk>', ProductDetail.as_view, name="product_detail"),
 ]
--- PennTrade/rest/views.py
+++ PennTrade/rest/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
+from rest_framework import generics
+from .serializers import ProductSerializer
+from .models import Product
 
 # Create your views here.
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetail(generics.RetrieveDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
